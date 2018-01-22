6f74a4901226728ba3ccd5d94fed4e915c075955
DESKTOP-LODH12D\Sandy <sandeepch32@gmail.com>
2018-01-22 11:20:50 -0500

    Hello world program to commit and push

--- Customer_Information/src/org/sandeep/customerinformation/HelloWold.java
+++ Customer_Information/src/org/sandeep/customerinformation/HelloWold.java
@@ -3,7 +3,7 @@ package org.sandeep.customerinformation;
 public class HelloWold {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Sample commit for hello world program");
 
 	}
 
